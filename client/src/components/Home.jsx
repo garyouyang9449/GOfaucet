24c1f7443adc361f547554eeccf91221b0a3d84a
garyouyang9449 <garyouyang9449@gmail.com>
2021-12-31 12:17:24 -0800

    connect to metamask wallet

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 import Header from './Header';
 import Ether from './Ether';
-
+import Web3 from 'web3'
 
 const Home = () => {
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    loadBlockChain();
+  }, []);
+
+  const loadBlockChain = async() => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
+    const accounts = await web3.eth.getAccounts();
+    setAccount({ account: accounts[0] });
+  }
 
   return (
     <>
