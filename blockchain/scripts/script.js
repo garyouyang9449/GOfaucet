9506297c6316c1007f0c7ce26841ae89f0cc4d72
garyouyang9449 <garyouyang9449@gmail.com>
2021-12-30 22:40:45 -0800

    added deployment contract

---
+++ blockchain/scripts/script.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Greeter.deploy("Done deploying!");
+
+  await faucet.deployed();
+
+  console.log("Faucet deployed to:", faucet.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
