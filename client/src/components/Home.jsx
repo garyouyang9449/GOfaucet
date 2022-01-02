d343dc37de031bbce1011bd550d1f1bd0f8ffda8
garyouyang9449 <garyouyang9449@gmail.com>
2022-01-01 21:19:02 -0800

    added local host contract abi and address

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,11 @@ import Header from './Header';
 import Ether from './Ether';
 import Web3 from 'web3'
 
+const jsonInterface = require('../constants/contractABI.json');
+var Contract = require('web3-eth-contract');
+
+const address = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+
 const Home = () => {
   const [account, setAccount] = useState('');
 
@@ -13,6 +18,9 @@ const Home = () => {
 
   const loadBlockChain = async() => {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
+    Contract.setProvider('http://127.0.0.1:8545/');
+    var contract = new Contract(jsonInterface, address);
+
     const accounts = await web3.eth.getAccounts();
     setAccount({ account: accounts[0] });
   }
